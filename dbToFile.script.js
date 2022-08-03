b7ddf05c9ad8becc638d3b96b3d5e1e8f031a57c
ptrickd <patrickdion8@gmail.com>
2022-08-03 13:12:27 -0600

    all the db get write to file on json format

--- dbToFile.script.js
+++ dbToFile.script.js
@@ -1,28 +1,57 @@
-//Scripts 
+//Script backing up a mongodb database to a json file
 
 const fs = require('fs')
 
-var db = connect('mongodb://localhost/test');
+var db = connect('mongodb://localhost/inventory-app');
 
 const dbToFile = () => {
-    var movies = db.movies.find()
 
-    let moviesObj = []
-    movies.forEach(({ title, year, genres }) => {
-        moviesObj.push({
-            title: title,
-            year: year,
-            genres: genres
+    //Products is not a array but a cursor
+    var products = db.products.find()
+
+    let productsArr = []
+    products.forEach((product) => {
+        productsArr.push({
+            name: product.name,
+            categoryId: product.categoryId,
+            currentAmount: product.currentAmount,
+            previousAmount: product.previousAmount,
+            userId: product.userId,
+            unit: product.unit
         })
     })
 
-    const writeToFile = (arg) => fs.writeFile('db.txt', arg, (err) => {
-        if (err) throw err
+    var users = db.users.find()
+    let usersArr = []
+    users.forEach((user) => {
+        usersArr.push({
+            email: user.email,
+            password: user.password
+        })
     })
 
-    writeToFile(JSON.stringify(moviesObj))
+    var categories = db.categories.find()
+    let categoriesArr = []
+    categories.forEach((category) => {
+        categoriesArr.push({
+            name: category.name,
+            userId: category.userId
+        })
+    })
+
+    let finalObj = {}
+    finalObj.products = productsArr
+    finalObj.users = usersArr
+    finalObj.categories = categoriesArr
+
+    //use stringify(data,null,4) to write a human readable format
+    writeToFile(JSON.stringify(finalObj, null, 4))
 
 }
+
+const writeToFile = (arg) => fs.writeFile('db.json', arg, (err) => {
+    if (err) throw err
+})
 
 dbToFile()
 
